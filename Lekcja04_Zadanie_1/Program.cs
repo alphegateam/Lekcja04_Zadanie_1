a14c4b8d40a3fc816ea5149333623fb48a87d7ea
manx <manx@DESKTOP-95I9G38>
2023-06-11 21:41:29 +0200

    Add project files.

---
+++ Lekcja04_Zadanie_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Zadanie_4_1
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            // Lekcja 4, zadanie dowowe 1.
+            string Imie;
+            string Nazwisko;
+            int Wiek;
+            char Plec;
+            int Pesel;
+            int NrPracownika;
+
+        }
+    }
+}
+
+
+  
